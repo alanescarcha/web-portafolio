ee31fb7ae7c80f5898856a41e9f1826ff598e577
Alan Escarcha <63257497+alanescarcha@users.noreply.github.com>
2021-03-05 20:48:38 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 window.addEventListener("load", function(){
     document.getElementById("loader").classList.toggle("loader2");
+    lottie.loadAnimation({
+      container: document.getElementById('element'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/data.json' // the path to the animation json
+      });
     console.log("Copyright © " + new Date().getFullYear() + " by Alan Escarcha");
 });
 function cargada(){
